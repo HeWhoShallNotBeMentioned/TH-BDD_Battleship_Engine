d9f6e172fafe73ab62b771d69788a265eeee79c7
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-21 20:07:17 -0800

    add test for ship with multiple coordinates

--- test/ship_test.js
+++ test/ship_test.js
@@ -5,13 +5,18 @@ describe('checkForShip', function() {
 
   it('should correctly report no ship at a given players coordinate', function (){
     player = {ships:[{locations: [[0,0]]}]};
-
     expect(checkForShip(player, [9,9])).to.be.false;
   });
 
   it('should correctly report a ship at a given players coordinate', function (){
     player = {ships:[{locations: [[5,0]]}]};
-
     expect(checkForShip(player, [5,0])).to.be.true;
+  });
+
+  it('should correctly report a ship at a given players with multiple coordinates', function (){
+    player = {ships:[{locations: [[5,0],[5,1]]}]};
+    expect(checkForShip(player, [5,0])).to.be.true;
+    expect(checkForShip(player, [5,1])).to.be.true;
+    expect(checkForShip(player, [9,9])).to.be.false;
   });
 });
