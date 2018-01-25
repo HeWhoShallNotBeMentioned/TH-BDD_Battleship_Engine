63c1ba4bb9c54d944e3b82e9f18622e709213275
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-24 23:47:02 -0800

    add asynchronis testing

--- test/game_test.js
+++ test/game_test.js
@@ -47,6 +47,23 @@ describe('Game Instance Functions', function () {
 		});
 	});
 
+  function saveGame (callback) {
+		setTimeout(function () {
+			callback();
+		}, 1000);
+	}
+
+	describe('saveGame', function () {
+		it('should update save status', function (done) {
+			var status = 'game not saved...';
+
+			saveGame(function () {
+				status = 'game saved!';
+				expect(status).to.equal('game saved!');
+				done();
+			});
+		});
+	});
 
 
 });
