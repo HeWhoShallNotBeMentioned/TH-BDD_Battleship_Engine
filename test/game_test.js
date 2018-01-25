43abbbb28f00d80f801e8ec969b0da452ef7c804
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-24 20:04:42 -0800

    add game_test.js to show how to set up tests that you are still working on

---
+++ test/game_test.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+
+describe('Game Instance Functions', function () {
+  describe('checkGameStatus', function (){
+    it('should tell me when the game is over');//pending test as it has no function with expectation/criteria. Can also put an x infront of a test to make it
+  });
+  xit('Something else to be tested', function (){
+
+  });//can even put x in from of describe blocks to make all tests pending
+});
