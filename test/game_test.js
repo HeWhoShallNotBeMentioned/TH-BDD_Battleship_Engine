74793f52ff7c630d9cba37606449bc03a9394f7b
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-24 20:33:55 -0800

    add watch to auto run tests upon saved chages to part of code being tested

--- test/game_test.js
+++ test/game_test.js
@@ -2,7 +2,24 @@ const expect = require('chai').expect;
 
 describe('Game Instance Functions', function () {
   describe('checkGameStatus', function (){
-    it('should tell me when the game is over');//pending test as it has no function with expectation/criteria. Can also put an x infront of a test to make it
+    var checkGameStatus = require('../game_logic/game_instance.js').checkGameStatus;
+    it('should tell me when the game is over', function () {
+      var players = [
+        {
+          ships: [
+            {
+              locations: [[0, 0]],
+              damage: [[0, 0]]
+            }
+          ]
+        }
+      ];
+      var actual = checkGameStatus(players);
+      expect(actual).to.be.false;
+
+    });//pending test as it has no function with expectation/criteria. Can also put an x infront of a test to make it
+
+
   });
   xit('Something else to be tested', function (){
 
